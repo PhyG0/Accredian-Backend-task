c32789963914fb19f5cfd6384db9a76b214d1fe3
PhyG0 <zebra9989zebra@gmail.com>
2024-07-06 22:51:53 +0530

    updated allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'https://accredian-frontend-task-cmfd02hip-phyg0s-projects.vercel.app',
   'https://accredian-frontend-task-tau-one.vercel.app/',
+  'http://localhost:5173',
 ]
 
 module.exports = allowedOrigins
