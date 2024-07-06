28cc8c84dc0369bcdcf741c00903e7bfcaf79575
Krishna <80216446+PhyG0@users.noreply.github.com>
2024-07-06 22:40:00 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'https://accredian-frontend-task-cmfd02hip-phyg0s-projects.vercel.app',
-  'https://accredian-frontend-task-tau-one.vercel.app/',
+  'https://accredian-frontend-task-tau-one.vercel.app',
 ]
 
 module.exports = allowedOrigins
